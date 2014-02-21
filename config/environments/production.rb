cb188b01f05e6f6cf5e0e6129a0cc2b3b3309a29
Jacinto Callu <jacinto@appstack.com>
2014-02-21 12:31:40 -0800

    rake file heroku error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Metrics::Application.configure do
+Crowdkillers::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
