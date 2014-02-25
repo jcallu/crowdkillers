c9c077ceac5fc9d944e43e269287470ec1cfabf6
Jacinto Callu <jacinto@appstack.com>
2014-02-25 15:07:11 -0800

    Fix config files

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
-Metrics::Application.routes.draw do
-  resources :sessions, only: [:new, :create, :destroy]
+Crowdkillers::Application.routes.draw do
+  resources :sessions, only: [ :create, :destroy, :new]
   match '/:action', controller: :pages
   root to: 'pages#index'
 end
